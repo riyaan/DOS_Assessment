a159755f55aa5e6d6b2d318b491a8f278b6cacbc
riyaan <riyaan.williams@gmail.com>
2018-01-10 23:33:40 +0200

    Added new file

---
+++ Test2/Tests/BlaiseTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test2.PascalsTriangle;
+
+namespace Test2.UnitTests
+{
+    [TestClass]
+    public class BlaiseTests
+    {
+        Blaise b;
+
+        private void Setup(int numberOfRows)
+        {
+            b = new Blaise(numberOfRows);
+        }
+
+        [TestMethod]
+        public void EvaluateColumn0Row2_Success()
+        {
+            Setup(2);
+
+            int result = b.Pascal(0, 2);
+            int expected = 1;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateColumn0Row2_Fail()
+        {
+            Setup(2);
+
+            int result = b.Pascal(0, 2);
+            int notExpected = 12;
+
+            Assert.AreNotEqual(notExpected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateColumn1Row2_Success()
+        {
+            Setup(2);
+
+            int result = b.Pascal(1, 2);
+            int expected = 2;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateColumn1Row2_Fail()
+        {
+            Setup(2);
+
+            int result = b.Pascal(1, 2);
+            int notExpected = 12;
+
+            Assert.AreNotEqual(notExpected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateColumn1Row3_Success()
+        {
+            Setup(3);
+
+            int result = b.Pascal(1, 3);
+            int expected = 2;
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void EvaluateColumn1Row3_Fail()
+        {
+            Setup(3);
+
+            int result = b.Pascal(1, 3);
+            int notExpected = 100;
+
+            Assert.AreNotEqual(notExpected, result);
+        }
+
+        #region Process Tests
+
+        [TestMethod]
+        public void Process_Success()
+        {
+            Setup(3);
+
+            b.Process();
+
+            Assert.IsTrue(true);
+        }
+
+        #endregion
+    }
+}
