e96bab4463587818b38245a586bfeb4f8795a877
riyaan <riyaan.williams@gmail.com>
2018-01-10 23:09:10 +0200

    Added unit tests

---
+++ Test2/PascalsTriangle/Blaise.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Test2.PascalsTriangle
+{
+    public class Blaise
+    {
+        private int rows;
+
+        public int Rows
+        {
+            get { return rows; }
+            set { rows = value; }
+        }
+
+        public Blaise(int rows)
+        {
+            Rows = rows;
+        }
+
+        public void Process()
+        {
+            for (int i = 0; i <= Rows; i++)
+            {
+                for (int j = Rows; j > i; j--)
+                {
+                    Console.Write("   ");
+                }
+
+                for (int k = 0; k <= i; k++)
+                {
+                    Console.Write(String.Format("{0, 6}", Pascal(i, k)));
+                }
+
+                Console.WriteLine("");
+            }
+        }
+
+        public int Pascal(int column, int row)
+        {
+            if ((column == 0 || row == 0 || column == row))
+            {
+                return 1;
+            }
+            else
+            {
+                return Pascal(column - 1, row - 1) + Pascal(column - 1, row);
+            }
+        }
+    }
+}
--- Test2/PascalsTriangle/Program.cs
+++ Test2/PascalsTriangle/Program.cs
@@ -1,44 +1,20 @@
 ﻿using System;
 
-namespace PascalsTriangle
+namespace Test2.PascalsTriangle
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number = ");
+            Console.WriteLine("Enter the number of rows:");
 
-            int num = 0;
+            int rows = 0;
 
-            if (Int32.TryParse(Console.ReadLine(), out num) == false)
+            if (Int32.TryParse(Console.ReadLine(), out rows) == false)
                 return;
 
-            for (int i = 0; i <= num; i++)
-            {
-                for (int j = num; j > i; j--)
-                { 
-                    Console.Write("   ");
-                }
-
-                for (int k = 0; k <= i; k++)
-                {
-                    Console.Write(String.Format("{0, 6}", pascal(i, k)));
-                }
-
-                Console.WriteLine("");
-            }
-        }
-
-        static int pascal(int column, int row)
-        {
-              if ((column == 0 || row == 0 || column == row)) 
-              {
-                return 1;
-              }
-              else
-              {
-                return pascal(column - 1, row - 1) + pascal(column - 1, row);
-              }
+            Blaise blaise = new Blaise(rows);            
+            blaise.Process();
         }
     }
 }
